84987c1db1627a37e3a667f39e110f8654e8b1a1
praburangki <praburangki@gmail.com>
2023-03-06 11:22:59 +0800

    chore: add menu entity

---
+++ app/menu/menu.entity.ts
@@ -0,0 +1,28 @@
+export enum MENU_MODE {
+  VERTICAL = 'vertical',
+  HORIZONTAL = 'horizontal',
+  VERTICAL_RIGHT = 'vertical-right',
+  INLINE = 'inline',
+}
+
+export enum MENU_TYPE {
+  // left menu
+  SIDEBAR = 'sidebar',
+
+  MIX_SIDEBAR = 'mix-sidebar',
+  // mixin menu
+  MIX = 'mix',
+  // top menu
+  TOP_MENU = 'top-menu',
+}
+
+export enum TRIGGER_TYPE {
+  NONE = 'NONE',
+  FOOTER = 'FOOTER',
+  HEADER = 'HEADER',
+}
+
+export enum MIX_SIDEBAR_TRIGGER {
+  HOVER = 'hover',
+  CLICK = 'click',
+}
