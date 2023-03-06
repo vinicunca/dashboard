e2de5579673a914d54ac42602f61cde8935301ef
praburangki <praburangki@gmail.com>
2023-03-06 11:21:54 +0800

    chore: add config entity

---
+++ app/config/entity.config.ts
@@ -0,0 +1,39 @@
+export enum THEME_TYPE {
+  DARK = 'dark',
+  LIGHT = 'light',
+}
+
+export enum SETTING_BUTTON_POSITION {
+  AUTO = 'auto',
+  HEADER = 'header',
+  FIXED = 'fixed',
+}
+
+export enum PERMISSION_MODE {
+  ROLE = 'ROLE',
+  ADMIN = 'ADMIN',
+  ROUTE_MAPPING = 'ROUTE_MAPPING',
+}
+
+export enum SESSION_TIMEOUT_PROCESSING {
+  ROUTE_JUMP,
+  PAGE_COVERAGE,
+}
+
+export enum CONTENT_TYPE {
+  // auto width
+  FULL = 'full',
+  // fixed width
+  FIXED = 'fixed',
+}
+
+// Route switching animation
+export enum ROUTER_TRANSITION {
+  ZOOM_FADE = 'zoom-fade',
+  ZOOM_OUT = 'zoom-out',
+  FADE_SIDE = 'fade-slide',
+  FADE = 'fade',
+  FADE_BOTTOM = 'fade-bottom',
+  FADE_SCALE = 'fade-scale',
+}
+
