a265c1c3f03c28e3479182973f73e52aacfd564a
praburangki <praburangki@gmail.com>
2023-03-07 11:22:54 +0800

    wip: add system store

--- app/system/system.store.ts
+++ app/system/system.store.ts
@@ -1,10 +1,14 @@
+import { type DeepPartial } from 'unocss';
 import { defineStore } from 'pinia';
+import { mergeDeep } from '@vinicunca/js-utilities';
 
-import { type MenuBeforeMiniState } from '../menu/menu.type';
-import { type ProjectConfig } from './system.type';
+import { type HeaderSetting, type MenuSetting, type MultiTabsSetting, type ProjectConfig, type TransitionSetting } from './system.type';
 import { type THEME_TYPE } from './system.entity';
+import { darkMode } from './system.design';
 
-import { PROJ_CFG_KEY } from '~~/app/cache/cache.entity';
+import { type MenuBeforeMiniState } from '~~/app/menu/menu.type';
+import { Persistent } from '~~/app/cache/persistent';
+import { APP_DARK_MODE_KEY_, PROJ_CFG_KEY } from '~~/app/cache/cache.entity';
 
 interface ConfigState {
   darkMode?: THEME_TYPE;
@@ -18,12 +22,84 @@ interface ConfigState {
 
 let timeId: TimeoutHandle;
 
-export const useAppStore = defineStore({
+export const useSystemStore = defineStore({
   id: 'app',
+
   state: (): ConfigState => ({
     darkMode: undefined,
     pageLoading: false,
     projectConfig: Persistent.getLocal(PROJ_CFG_KEY),
     menuBeforeMini: {},
   }),
+
+  getters: {
+    getPageLoading(): boolean {
+      return this.pageLoading;
+    },
+    getDarkMode(): 'light' | 'dark' | string {
+      return this.darkMode || localStorage.getItem(APP_DARK_MODE_KEY_) || darkMode;
+    },
+
+    getBeforeMiniInfo(): MenuBeforeMiniState {
+      return this.menuBeforeMini;
+    },
+
+    getProjectConfig(): ProjectConfig {
+      return this.projectConfig || ({} as ProjectConfig);
+    },
+
+    getHeaderSetting(): HeaderSetting {
+      return this.getProjectConfig.headerSetting;
+    },
+
+    getMenuSetting(): MenuSetting {
+      return this.getProjectConfig.menuSetting;
+    },
+
+    getTransitionSetting(): TransitionSetting {
+      return this.getProjectConfig.transitionSetting;
+    },
+
+    getMultiTabsSetting(): MultiTabsSetting {
+      return this.getProjectConfig.multiTabsSetting;
+    },
+  },
+
+  actions: {
+    setPageLoading(loading: boolean): void {
+      this.pageLoading = loading;
+    },
+
+    setDarkMode(mode: THEME_TYPE): void {
+      this.darkMode = mode;
+      localStorage.setItem(APP_DARK_MODE_KEY_, mode);
+    },
+
+    setMenuBeforeMini(state: MenuBeforeMiniState): void {
+      this.menuBeforeMini = state;
+    },
+
+    setProjectConfig(config: DeepPartial<ProjectConfig>): void {
+      this.projectConfig = (mergeDeep(this.projectConfig || {}, config) as ProjectConfig);
+      Persistent.setLocal({ key: PROJ_CFG_KEY, value: this.projectConfig });
+    },
+
+    async resetAllState() {
+      resetRouter();
+      Persistent.clearAll();
+    },
+
+    async setPageLoadingAction(loading: boolean): Promise<void> {
+      if (loading) {
+        clearTimeout(timeId);
+        // Prevent flicker
+        timeId = setTimeout(() => {
+          this.setPageLoading(loading);
+        }, 50);
+      } else {
+        this.setPageLoading(loading);
+        clearTimeout(timeId);
+      }
+    },
+  },
 });
